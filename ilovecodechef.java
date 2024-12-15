e9b156e452ba6bfc8646315605d6bbc99f6983e3
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-15 09:48:09 +0530

    Add files via upload

---
+++ ilovecodechef.java
@@ -0,0 +1,7 @@
+public class ilovecodechef{
+    public static void main(String[] args) {
+        System.out.println("I");
+        System.out.println("love");
+        System.out.println("CodeChef");
+    }
+}
